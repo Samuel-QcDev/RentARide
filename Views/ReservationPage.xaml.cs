48196ef75991b71be925648b8e8c1a1e035b66fc
Samuel <slassonde@gmail.com>
2023-11-05 18:04:48 -0500

    Basic Reservation & Result pages + Navigation

--- Views/ReservationPage.xaml.cs
+++ Views/ReservationPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class ReservationPage : ContentPage
     {
         InitializeComponent();
     }
+
+private void Search_Clicked(object sender, EventArgs e)
+{
+    Navigation.PushAsync(new ResultPage());
+}
 }
