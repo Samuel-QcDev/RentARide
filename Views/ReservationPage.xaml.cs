fd6dd4d7b5cb6cc7dbe161df2a9e9f8256f2309d
Samuel <slassonde@gmail.com>
2023-11-26 17:20:46 -0500

    Fixed new class issue

--- Views/ReservationPage.xaml.cs
+++ Views/ReservationPage.xaml.cs
@@ -12,18 +12,11 @@ public partial class ReservationPage : ContentPage
     public string stationId;
     private int optionsCode;
 
-    public ReservationPage(DateTime date, int hreDebut, int minsDebut,
-            int hreFin, int minsFin, string type, string station,
-            [Optional] string categorie, [Optional] int options)
+    public ReservationPage()
     {
         InitializeComponent();
 
-        startTime = new DateTime(date.Year, date.Month, date.Day, hreDebut, minsDebut, 0);
-        endTime = new DateTime(date.Year, date.Month, date.Day, hreFin, minsFin, 0);
-        typeVehicule = type;
-        categorieAuto = categorie;
-        stationId = station;
-        optionsCode = options;
+      
 
     }
 
@@ -32,43 +25,43 @@ public partial class ReservationPage : ContentPage
         Navigation.PushAsync(new ResultPage());
     }
 
-    public static void CreerMembre(string membreId, string name, string categorie)
-    {
-
-    }
-    public static void CreerVehicule()
-    {
-
-    }
-    public static void CreerStation(string stationId, string address, string parkSpaces)
-    {
-
-    }
-    private class Membre
-    {
-        private string membreId;
-        private string name;
-        private string categorie;
-        public Membre()
+            public static void CreerMembre(string membreId, string name, string categorie)
+        {
+            
+        }
+        public static void CreerVehicule()
         {
 
         }
-    }
-    private class Vehicule
-    {
-        private string vehiculeId;
-        private string categorieAuto;
-        private string type;
-        public Vehicule()
+        public static void CreerStation(string stationId, string address, string parkSpaces)
         {
+            
+        }
+        private class Membre
+        {
+            private string membreId;
+            private string name;
+            private string categorie;
+            public Membre()
+            {
+                    
+            }
+        }
+        private class Vehicule
+        {
+            private string vehiculeId;
+            private string categorieAuto;
+            private string type;
+            public Vehicule()
+            {
+
+            }
+        }
+        public class Station
+        {
+            private string station;
+            public string address;
+            private string parkSpaces;
 
         }
-    }
-    public class Station
-    {
-        private string station;
-        public string address;
-        private string parkSpaces;
-
-    }
 }
