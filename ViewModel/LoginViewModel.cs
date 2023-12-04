ffdf6264f102362c44b11e2aedb3d825df022f54
Samuel Lassonde <slassonde@gmail.com>
2023-12-03 19:22:57 -0500

    Login Page : Submit command

--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -8,7 +8,22 @@ namespace RentARide.ViewModel;
 
 [ObservableObject]
 public partial class LoginViewModel
-{
-        [ObservableProperty] private string name;
-        [ObservableProperty] private string password;
+{ 
+    [ObservableProperty] private string name;
+    [ObservableProperty] private string password;
+
+    [RelayCommand]
+    private async void Submit()
+        {
+            for (var i = 0.0; i < 1.0; i += 0.1)
+            {
+                await LoginPage.LoginProgressBar.ProgressTo(i, 500, Easing.Linear);
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+
+                "Submit",
+                $"You entered {Name} and {Password}",
+                "OK");
+        }
     }
--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -12,14 +12,19 @@ namespace RentARide.Views;
 
 public partial class LoginPage : ContentPage
 {
+    private double LoginProgress { get; set; }
+    public static ProgressBar LoginProgressBar;
+
     LoginViewModel vm = new LoginViewModel();
     public LoginPage()
 	{
+        LoginProgressBar = new ProgressBar();
         BindingContext = vm;
         InitializeComponent();
-	}
+        LoginStackLayout.Children.Add(LoginProgressBar);
+    }
     // Temporary method for Submit button, will be changed to a command
-    private void Submit_Clicked(object sender, EventArgs e)
+    private void Forgot_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new MainPage());
     }
